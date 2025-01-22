46d3f649c656cafb2ef87c29a9b79fb8f5aa1b5f
Umapathi2004 <umapathi1014@gmail.com>
2025-01-22 13:39:38 +0530

    change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
-import { Routes } from '@angular/router';
+import { Routes,RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
 import { SplashScreenComponent } from './components/splash-screen/splash-screen.component'
 import { HomeScreenComponent } from './components/home-screen/home-screen.component';
 
-const routes: Routes = [
+export const routes: Routes = [
   { path: '', component: SplashScreenComponent },  
-  { path: 'home', component: HomeScreenComponent } 
+  { path: 'home', component:HomeScreenComponent}
 ];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { SplashScreenComponent } from './components/splash-screen/splash-screen.component'
-import { HomeScreenComponent } from './components/home-screen/home-screen.component';
-
-export const routes: Routes = [
-  { path: '', component: SplashScreenComponent },  
-  { path: 'home', component: HomeScreenComponent } 
-];
